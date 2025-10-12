27f112669d28bafbc087de27e5a97a554675de16
Trung Pham <pbtrung@outlook.com>
2025-10-12 23:20:27 +0700

    Fix filename

--- dynsnap/entrypoint.sh
+++ dynsnap/entrypoint.sh
@@ -9,11 +9,10 @@ while true; do
     line=$(shuf -n 1 /music/db01.txt)
     first=$(echo "$line" | cut -d',' -f1)
     second=$(echo "$line" | cut -d',' -f2)
-    binname=${first##*/}
-    fname="${binname%.bin}.opus"
+    fname=${first##*/}
     fullname="/music/downloads/$fname"
 
-    rclone --config /music/rclone.conf copy $first $fullname -v --stats 5s
+    rclone --config /music/rclone.conf copy $first /music/downloads/ -v --stats 5s
     dynaudnorm --input-bits 16 --input-chan 2 --input-rate 48000 \
         -f 500 -g 31 -p 0.95 -m 8 -r 0.22 -s 25.0 \
         -d libopusfile -t raw \
