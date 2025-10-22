1585602f789b0283eb7a74a5042ac91ce1656e2a
Trung Pham <pbtrung@outlook.com>
2025-10-22 22:39:39 +0700

    Use playbin3

--- dynsnap/entrypoint.sh
+++ dynsnap/entrypoint.sh
@@ -118,16 +118,12 @@ play_track() {
     local fullname="$1"
     
     set -o pipefail
-    gst-launch-1.0 -e -t playbin3 uri=file://"$fullname" \
-        audio-sink="audioresample ! audioloudnorm loudness-target=-16.0 ! \
-                    audioresample ! audioconvert ! \
-                    audio/x-raw,rate=48000,channels=2,format=S16LE ! filesink location=$SNAPFIFO" \
-        1>"$INFOFIFO" &
-    PIPELINE_PID=$!
-    
-    if ! wait $PIPELINE_PID; then
+    if ! gst-launch-1.0 -e -t playbin3 uri=file://"$fullname" \
+            audio-sink="audioresample ! audioloudnorm loudness-target=-16.0 ! \
+                        audioresample ! audioconvert ! \
+                        audio/x-raw,rate=48000,channels=2,format=S16LE ! filesink location=$SNAPFIFO" \
+            1>"$INFOFIFO" 2>&1; then
         echo "Error: pipeline failed for $fullname"
-        kill_pipeline
         rm -f "$fullname"
         return 1
     fi
