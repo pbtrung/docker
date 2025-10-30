576adfc5216b40dbedc13ae12a2278eb6a1b6b1a
Trung Pham <pbtrung@outlook.com>
2025-10-30 20:06:42 +0700

    Move -re

--- dynsnap/entrypoint.sh
+++ dynsnap/entrypoint.sh
@@ -103,11 +103,11 @@ play_track() {
 
     log_message "Streaming to Icecast: $fullname"
 
-    # Single ffmpeg does decode -> normalize -> encode -> Icecast
     if ! ffmpeg -nostdin -hide_banner -i "$fullname" \
         -af "dynaudnorm=f=500:g=31:p=0.95:m=8:r=0.22:s=25.0" \
         -ar 48000 -sample_fmt s16 -ac 2 \
         -c:a flac -compression_level 6 \
+        -re \
         -f ogg -content_type application/ogg \
         icecast://source:hackme@localhost:8000/stream.ogg \
         2>"$INFOFIFO"; then
