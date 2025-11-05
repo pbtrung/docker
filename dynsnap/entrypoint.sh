8324d3cc893477e34654033d8db1be59626b94bf
Trung Pham <954373+pbtrung@users.noreply.github.com>
2025-11-05 15:38:18 +0700

    Revert to snapcast

--- dynsnap/entrypoint.sh
+++ dynsnap/entrypoint.sh
@@ -142,10 +142,7 @@ play_track() {
     
     if ! ffmpeg -nostdin -hide_banner -y -i "$fullname" \
         -af "dynaudnorm=f=500:g=31:p=0.95:m=8:r=0.22:s=25.0" \
-        -ar 48000 -sample_fmt s16 -ac 2 \
-        -c:a flac -compression_level 6 \
-        -f ogg -content_type application/ogg -listen 2 \
-        http://localhost:8888/music 2>"$INFOFIFO"
+        -f s16le -ar 48000 -ac 2 "$SNAPFIFO" 2>"$INFOFIFO"
     then
         log_message "Error: ffmpeg streaming failed"
         rm -f "$fullname"
